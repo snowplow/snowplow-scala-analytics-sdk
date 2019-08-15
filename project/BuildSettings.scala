0378a73acb25484a4eef3d8256a0595051e81e39
Enes Aldemir <aldemirenes@live.com>
2019-08-15 15:28:31 +0300

    Integrate scoverage

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -23,6 +23,9 @@ import bintray.BintrayKeys._
 import com.typesafe.tools.mima.plugin.MimaKeys._
 import com.typesafe.tools.mima.plugin.MimaPlugin
 
+// Scoverage plugin
+import scoverage.ScoverageKeys._
+
 object BuildSettings {
 
   // Basic settings for our app
@@ -76,6 +79,16 @@ object BuildSettings {
     test in Test := {
       mimaReportBinaryIssues.value
       (test in Test).value
+    }
+  )
+
+  val scoverageSettings = Seq(
+    coverageEnabled := true,
+    coverageMinimum := 50,
+    coverageFailOnMinimum := true,
+    coverageHighlighting := false,
+    (test in Test) := {
+      (coverageReport dependsOn (test in Test)).value
     }
   )
 }
